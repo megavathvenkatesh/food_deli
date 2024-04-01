b2c4c6125a85098a3b5d62dcd15477d22413ce86
venka <venkateshmegavath75@gmail.com>
2024-04-01 08:24:29 +0530

    Initial commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Food delivery platforms connect users with restaurants, offering diverse cuisines for delivery or pickup. Payment is online, fees may apply, and features like order tracking enhance convenience.</p>
+                <button>View Menu</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,56 @@
+// import React, { useState } from "react";
+// import './LoginPopup';
+
+// const LoginPopup = ({setShowLogin}) => {
+    
+//     const [currState, setCurrState] = useState("Sign Up")
+//     return(
+//         <div className='login-popup'>
+//             <div className='login-popup-container'>
+//                 <div className='login-popup-title'>
+//                     <h2>{currState}</h2>
+//                     <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default LoginPopup;
+
+
+import React, { useState } from "react";
+import './LoginPopup.css';
+import { assets } from "../../assets/assets"; // Adjust the path accordingly
+
+const LoginPopup = ({ setShowLogin }) => {
+    
+    const [currState, setCurrState] = useState("Login");
+    return(
+        <div className='login-popup'>
+            <div className='login-popup-container'>
+                <div className='login-popup-title'>
+                    <h2>{currState}</h2>
+                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+                </div>
+                <div className="login-popup-inputs">
+                   {currState==='Login'?<></>:<input type="text" placeholder='Your name' required/>}
+                    <input type="email" placeholder='Your email' required/>
+                    <input type="password" placeholder='password' required/>
+                </div>
+                <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+                <div className="login-popup-condition">
+                    <input type="checkbox" required/>
+                    <p>By contunuing, I agree to the terms of use & privacy policy.</p>
+                </div>
+                {currState==='Login'
+                ?<p>Create a new account?<span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+                :<p>Already have an account?<span onClick={()=>setCurrState("Login")}>Login here</span></p>
+                }             
+                
+            </div>
+        </div>
+    );
+};
+
+export default LoginPopup;
